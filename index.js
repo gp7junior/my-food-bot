7beefa69952d98faea0f051d25a166574b04f39c
Gilberto Pedro da Silva Junior <gp7junior@gmail.com>
2018-09-04 10:49:35 +0200

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express();
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(bodyParser.json())
+   .use(express.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .get('/', (req, res) => res.render('pages/index'))
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// Recast will send a post request to /errors to notify important errors
+// described in a json body
+app.post('/errors', (req, res) => {
+   console.error(req.body);
+   res.sendStatus(200);
+});
