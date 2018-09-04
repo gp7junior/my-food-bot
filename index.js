9dcddd6b26dfb42141c38e129a31936eb8fdddd3
Gilberto Pedro da Silva Junior <gp7junior@gmail.com>
2018-09-04 13:32:23 +0200

    organizing stuffs (putting the controllers inside the controllers 2)

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000
 const app = express();
 app.use(bodyParser.json())
    .use(express.static(path.join(__dirname, 'public')))
+   .use(express.static(path.join(__dirname, 'controllers')))
    .set('views', path.join(__dirname, 'views'))
-   .set('controllers', path.join(__dirname, 'controllers'))
    .set('view engine', 'ejs')
    .get('/', (req, res) => res.render('pages/index'))
    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
