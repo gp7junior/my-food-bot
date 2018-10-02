a8d4ad9d3505007eb4e4e3cb2db471b967a98b3f
pedro <gp7junior@gmail.com>
2018-10-02 11:00:04 +0200

    making tests with the sparql-client-2 module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const request = require('request');
+const request = require('request'); // reconsider?
+const {SparqlClient, SPARQL} = require('sparql-client-2');
 const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -12,6 +13,11 @@ app.use(bodyParser.json())
    .get('/', (req, res) => res.render('pages/index'))
    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+const client = new SparqlClient('https://362a6880.ngrok.io/my_food/sparql')
+   .register(
+     db: 'https://362a6880.ngrok.io/my_food/'
+   );
+
 // Recast will send a post request to /errors to notify important errors
 // described in a json body
 app.post('/errors', (req, res) => {
@@ -23,18 +29,16 @@ app.post('/errors', (req, res) => {
 //app.post('/find-restaurant', findRestaurants);
 app.post('/find-restaurant', (req, res) => {
 
-    const options = {
-      url: 'https://362a6880.ngrok.io/my_food/sparql',
-      method: 'POST',
-      headers: {
-        'query': 'SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} LIMIT 25'
-      }
-    };
+    // const options = {
+    //   url: 'https://362a6880.ngrok.io/my_food/sparql',
+    //   method: 'POST',
+    //   headers: {
+    //     'query': 'SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} LIMIT 25'
+    //   }
+    // };
 
-    request(options, function(err, res, body) {
-      let json = JSON.parse(body);
-      console.log(json);
-    });
+    client.query( SPARQL 'SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} LIMIT 25')
+      .execute();
 
     res.json({
       replies: [
