66cfe9012e070caf560175dd13ed9360476be3f8
pedro <gp7junior@gmail.com>
2018-11-08 18:08:31 +0100

    trying with request promise 2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/find-restaurant', (req, res) => {
     const oprtions = {
       method: 'GET',
       uri: sparql_enpoint,
-      qs: {'PREFIX myfood: <http://www.semanticweb.org/gp7junior/ontologies/2018/6/my-food-ontology#> SELECT ?subject ?object WHERE { ?subject rdf:type myfood:Restaurant }'}
+      qs: 'PREFIX myfood: <http://www.semanticweb.org/gp7junior/ontologies/2018/6/my-food-ontology#> SELECT ?subject ?object WHERE { ?subject rdf:type myfood:Restaurant }'
     }
     request(options)
       .then( (query_result) => {
