af4465971906994a6cf0c050419fb9369afffebc
pedro <gp7junior@gmail.com>
2018-11-19 13:45:34 +0100

    updating with some graql queries

--- controllers/findRestaurants.js
+++
@@ -1,8 +0,0 @@
-function findRestaurants(req, res) {
-  console.log("[POST] /test");
-  res.json({
-    replies: [
-      { type: 'text', content: `I still have to implement the skill find restaurant` },
-    ],
-  });
-}
